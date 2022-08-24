dc07357ff0097dd74fd97c23fe45cbe36d3f5e89
Aidan Sojo <agsojo@ad.unc.edu>
2022-08-24 00:31:32 -0400

    Progress on Exercise 1

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -1,9 +1,9 @@
 """EX01 - Chardle - A cute step towards Wordle!"""
 
-__author__: str = "730577804"
+__author__ = "730577804"
 
 word: str = input("Enter a 5-character word: ")
-matches: int = 0
+MATCHES: int = 0
 
 if len(word) == 5:
     character: str = input("Enter a single character: ")
@@ -11,27 +11,27 @@ if len(word) == 5:
         print("Searching for " + character + " in " + word)
         if word[0] == character:
             print(character + " found at index 0")
-            matches = matches + 1
+            MATCHES = MATCHES + 1
         if word[1] == character:
             print(character + " found at index 1")
-            matches = matches + 1
+            MATCHES = MATCHES + 1
         if word[2] == character:
             print(character + " found at index 2")
-            matches = matches + 1
+            MATCHES = MATCHES + 1
         if word[3] == character:
             print(character + " found at index 3")
-            matches = matches + 1
+            MATCHES = MATCHES + 1
         if word[4] == character:
             print(character + " found at index 4")
-            matches = matches + 1
-        if matches == 0:
+            MATCHES = MATCHES + 1
+        if MATCHES == 0:
             print("No instances of " + character + " found in " + word)
             exit()       
-        if matches > 1:
-            print(str(matches) + " instances of " + character + " found in " + word)
+        if MATCHES > 1:
+            print(str(MATCHES) + " instances of " + character + " found in " + word)
             exit()
-        if matches == 1:
-            print(str(matches) + " instance of " + character + " found in " + word)
+        if MATCHES == 1:
+            print(str(MATCHES) + " instance of " + character + " found in " + word)
             exit()
     else:
         print("Error: Character must be a single letter.")
