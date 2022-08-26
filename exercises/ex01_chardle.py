9a1684a80fe1c1afce971b181bbe8aad19517ffd
Aidan Sojo <agsojo@ad.unc.edu>
2022-08-26 16:28:57 -0400

    test 4

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -6,3 +6,14 @@ five_word: str = input("Enter a 5-character word: ")
 one_character: str = input("Enter a single character: ")
 
 print("Searching for " + one_character + " in " + five_word)
+
+if five_word[0] == one_character:
+    print(one_character + " found at index 0")
+if five_word[1] == one_character:
+    print(one_character + " found at indext 1")
+if five_word[2] == one_character:
+    print(one_character + " found at indext 2")
+if five_word[3] == one_character:
+    print(one_character + " found at indext 3")
+if five_word[4] == one_character:
+    print(one_character + " found at indext 4")
