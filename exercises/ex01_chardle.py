a33d31550ad3cfd98a878425f73782e1e3d459ac
Aidan Sojo <agsojo@ad.unc.edu>
2022-08-26 16:24:39 -0400

    test 3

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -5,7 +5,4 @@ __author__ = "730577804"
 five_word: str = input("Enter a 5-character word: ")
 one_character: str = input("Enter a single character: ")
 
-print("Searching for " + five_word + " in " + one_character)
-
-if five_word[0] == one_character:
-    print
+print("Searching for " + one_character + " in " + five_word)
