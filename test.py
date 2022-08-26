7acaf18f34299964b8726d7eca79ab9f46b9ded5
Aidan Sojo <agsojo@ad.unc.edu>
2022-08-26 16:03:03 -0400

    test 1

---
+++ test.py
@@ -0,0 +1,8 @@
+"""EX01 - Chardle - A cute step towards Wordle!"""
+
+__author__ = "730577804"
+
+five_word: str = input("Enter a 5-character word: ")
+one_character: str = input("Enter a single character: ")
+
+print("Searching for " + five_word + " in " + one_character)
