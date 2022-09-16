de01a22375cfba7d6d0db511aae8a160ad9d4906
Aidan Sojo <agsojo@ad.unc.edu>
2022-09-15 23:19:55 -0400

    progress on exercise 4

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -1,43 +1,43 @@
-"""Docstring doesn't have to mean anything all autograded!"""
+"""Recreating functions in Python."""
 
 __author__ = "730577804"
 
 
-def all(x: list[int], y: int) -> bool:
-    """Input a list and int to check if every element in list is equal to int."""
-    if len(x) == 0:
-        return False
+def all(input_list: list[int], num: int) -> bool:
+    """Input a list and int to check if every element in list is equal to specific int."""
+    if len(input_list) == 0:
+        return False  # result of an empty list
     i: int = 0
-    while i < len(x):
-        if x[i] == y:
+    while i < len(input_list):
+        if input_list[i] == num:
             i += 1
         else:
             return False
     return True
 
 
-def max(a: list[int]) -> int:
+def max(user_list: list[int]) -> int:
     """Input a list and its max is output."""
-    if len(a) == 0:
-        raise ValueError("max() arg is an empty List")
+    if len(user_list) == 0:
+        raise ValueError("max() arg is an empty List")  # result of an empty list
     i: int = 0
-    b = a[i]
+    b = user_list[i]  # variable that takes on the value of the max element of a list
     i += 1
     while i < len(a):
-        if b <= a[i]:
-            b = a[i]
+        if b <= a[i]:  # compares the most recent max value with the next element in the list
+            b = a[i]  # b takes on the value of the compared element if it is smaller
         i += 1
-    return b
+    return b  # max value
 
 
-def is_equal(moo: list[int], woof: list[int]) -> bool:
+def is_equal(one_list: list[int], two_list: list[int]) -> bool:
     """Compares the elements of two lists to determine if they are equal."""
-    if len(moo) != len(woof):
-        return False
+    if len(one_list) != len(two_list):
+        return False  # result if two lists aren't of equal length
     i: int = 0
-    while i < len(moo):
-        if moo[i] == woof[i]:
+    while i < len(one_list):
+        if one_list[i] == two_list[i]:
             i += 1
         else:
-            return False
-    return True
+            return False  # result if lists have a different element
+    return True  # result if the lists are equal
